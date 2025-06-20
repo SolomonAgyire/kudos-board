8bdb8330f97ee29ac4f3253e1647818b7d5fea51
Solomon Agyire <kingsolomon@meta.com>
2025-06-20 14:53:04 -0700

    Fix merge conflict in config/api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,3 @@
 export const API_CONFIG = {
-<<<<<<< HEAD
   BASE_URL: import.meta.env.VITE_API_URL || 'https://kudos-board-z9m6.onrender.com',
-=======
-    BASE_URL:"https://kudos-board-z9m6.onrender.com" 
->>>>>>> 8c2c2636dab6959cea187762849b9caa6a06747a
 };
